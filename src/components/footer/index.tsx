239418ad80f6c1cccb4269b62476e2a654c1d8fe
Moisés Sebastião <moisessebastiao2005@gmail.com>
2025-03-07 23:47:28 +0100

    feat: footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import { Container } from "../container"
 export function Footer() {
     return(
         <Container>
-        <div className="flex justify-between text-[#6C6C6C] text-[14px]">
+        <div className="flex sm:flex-row sm:justify-between flex-col sm:text-left text-center text-[#6C6C6C] text-[14px]">
             <div>Travelgram © 2024</div>
             <ul className="flex gap-3">
                 <li>Termos de uso</li>
