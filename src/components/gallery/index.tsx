75be3692fb95142da2a8903610d57c17125c08ef
Moisés Sebastião <moisessebastiao2005@gmail.com>
2025-03-08 00:07:37 +0100

    feat: search

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -5,9 +5,15 @@ export function Gallery({data}: any) {
         <Container>
         <div className="flex flex-wrap flex-1 sm:justify-between justify-center">
             {
-                data.map((item: any) => (
-                    <img src={item.rel} key={item.id} className="mb-4 w-[350px]"/>
-                ))
+                data.length !== 0 ? (                    
+                    data.map((item: any) => (
+                        <img src={item.rel} key={item.id} className="mb-4 w-[350px]"/>
+                    ))
+                ):(
+                <div className="w-full flex justify-center items-center">
+                    <p className="text-center">Imagem não encontrada...</p>
+                </div>
+                )
             }
         </div>
         </Container>
